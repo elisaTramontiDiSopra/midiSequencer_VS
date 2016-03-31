8d316724081995bd469a1163cd7b1df5b944462f
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2016-03-31 15:16:58 +0200

    PYTHON: fixed mqtt client. ora ascolta

--- mqttClientRead.py
+++ mqttClientRead.py
@@ -7,21 +7,17 @@ topic = "midiSequencerDiomede"
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    print("Subscribe on topic {}" + topic)
+    print("Subscribed topic:" + topic)
     client.subscribe(topic)
 
 def on_message(client, userdata, msg):
-    print ("Topic",msg.topic+'Messaggio: '+str(msg.payload))
-
-
-
+    print("Topic",msg.topic+'Messaggio: '+str(msg.payload))
 
 #initialize MQTT
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("broker.hivemq.com", porta, "midiSequencerDiomede")
-client.loop_start()
+client.connect("broker.hivemq.com", 1883, 60)
+client.loop_forever()

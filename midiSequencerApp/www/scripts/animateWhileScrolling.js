f30ff848f428e58d48a3bb65509042c35784dfb1
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2016-04-03 00:43:17 +0200

    CSS: nuova versione gialla

---
+++ midiSequencerApp/www/scripts/animateWhileScrolling.js
@@ -0,0 +1,27 @@
+﻿$(window).scroll(function () {
+    $('#nuovaCanzone').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+
+        if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
+            $(this).addClass("animated bounceInDown");
+        } else {
+            $(this).removeClass("animated bounceInDown");
+        }
+    });
+});
+
+$(window).scroll(function () {
+    $('#archivioCanzoni').each(function () {
+        var imagePos = $(this).offset().top;
+        var imageHeight = $(this).height();
+        var topOfWindow = $(window).scrollTop();
+
+        if (imagePos < topOfWindow + imageHeight && imagePos + imageHeight > topOfWindow) {
+            $(this).addClass("animated bounceInDown");
+        } else {
+            $(this).removeClass("animated bounceInDown");
+        }
+    });
+});
